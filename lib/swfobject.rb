940ff11f7b7e35b8726fc648be37b2fc379a8b87
Alex K <flskif@gmail.com>
2010-09-07 22:08:21 +0300

    Added static publishing method to swfobject

--- lib/swfobject.rb
+++ lib/swfobject.rb
@@ -7,7 +7,8 @@ class Swfobject
                 :height, 
                 :flashplayer_version, 
                 :params, 
-                :flashvars
+                :flashvars,
+                :use_static_publishing_method
   
   def initialize
     @src ||= "no_swf_defined.swf"
@@ -16,14 +17,43 @@ class Swfobject
     @flashplayer_version ||= "9.0.0"
     @params ||= {:quality => "best", :loop => "false", :menu => "false", :scale => "noscale", :salign => "tl"}
     @flashvars ||= {}
+    @use_static_publishing_method = false
   end
   
   def get(alternate_content = "")
     id = generate_id
-
     alternate_content = %{You need Flash Player #{flashplayer_version} +} if alternate_content == ""
-      
-    code = %{
+    use_static_publishing_method ? 
+        get_static_publishing_code(id, alternate_content) 
+        : 
+        get_dynamic_publishing_code(id, alternate_content)
+  end
+  
+private
+
+  def get_static_publishing_code(id, alternate_content)
+    flash_vars_query_string = @flashvars.to_query
+%{
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="#{width}" height="#{height}" id="#{id}" align="middle">
+	<param name="movie" value="#{src}" />
+	#{get_html_params}
+	<param name="flashvars" value="#{flash_vars_query_string}" />
+	<!--[if !IE]>-->
+	<object type="application/x-shockwave-flash" data="#{src}" width="#{width}" height="#{height}">
+		<param name="movie" value="#{src}" />
+  	#{get_html_params}
+		<param name="flashvars" value="#{flash_vars_query_string}" />
+	<!--<![endif]-->
+#{alternate_content}
+	<!--[if !IE]>-->
+	</object>
+	<!--<![endif]-->
+</object>
+}
+  end
+  
+  def get_dynamic_publishing_code(id, alternate_content)
+%{
 <div id="#{id}">
   #{alternate_content}
 </div>
@@ -34,10 +64,16 @@ class Swfobject
 #{js_object_from_hash "flashvars", flashvars}
 swfobject.embedSWF("#{src}", "#{id}", "#{width}", "#{height}", "#{flashplayer_version}", "#{express_install_src}", flashvars, params);
 </script>
-}
+}    
   end
   
-private
+  def get_html_params
+    code = ""
+    @params.each_pair do |name, value|
+      code << %{<param name="#{name}" value="#{value}" />}
+    end
+    code
+  end
   
   def js_object_from_hash(name, hash)
     code = %{var #{name} = {};
--- radiant-flash_content-extension.gemspec
+++ radiant-flash_content-extension.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{radiant-flash_content-extension}
-  s.version = "0.1.0"
+  s.version = "0.1.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Alex K"]
-  s.date = %q{2010-09-06}
+  s.date = %q{2010-09-07}
   s.description = %q{Allows you to insert flash content}
   s.email = %q{flskif@gmail.com}
   s.extra_rdoc_files = [
